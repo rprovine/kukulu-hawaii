1e831ac4b42ded7405897ca3cab3f35bde3203fa
rprovine <rprovine@gmail.com>
2025-02-27 18:10:42 -1000

    Update ESLint configuration and dependencies

---
+++ kukulu-site/app/page.tsx
@@ -0,0 +1,11 @@
+import ClientHeader from "@/components/client-header";
+import HowItWorks from "@/components/how-it-works";
+
+export default function Home() {
+  return (
+    <main>
+      <ClientHeader />
+      <HowItWorks />
+    </main>
+  );
+}
